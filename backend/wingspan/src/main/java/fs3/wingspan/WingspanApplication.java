b214cd07a834dd234309fc7866ba4eb397a386a8
taysoupa <tbauer1@iastate.edu>
2026-02-03 12:01:39 -0600

    adding in project init

---
+++ backend/wingspan/src/main/java/fs3/wingspan/WingspanApplication.java
@@ -0,0 +1,13 @@
+package fs3.wingspan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WingspanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WingspanApplication.class, args);
+	}
+
+}
---
+++ backend/wingspan/src/test/java/fs3/wingspan/WingspanApplicationTests.java
@@ -0,0 +1,13 @@
+package fs3.wingspan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WingspanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
