ecc5894e695dc2224addccc162ca64716939a956
ajvdb <ajvdb22@iastate.edu>
2026-02-09 21:58:43 -0600

    Set up for image controller and repository

--- backend/wingspan/src/main/java/fs3/wingspan/controller/ImageController.java
+++ backend/wingspan/src/main/java/fs3/wingspan/controller/ImageController.java
@@ -1,4 +1,19 @@
 package fs3.wingspan.controller;
 
+import fs3.wingspan.model.*;
+import fs3.wingspan.repository.ImageRepository;
+import fs3.wingspan.repository.TagRepository;
+import fs3.wingspan.repository.UserRepository;
+import fs3.wingspan.repository.APIKeyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/images")
 public class ImageController {
+
+    @Autowired
+    ImageRepository imageRepository;
+    @Autowired
+    private UserRepository userRepository;
 }
--- backend/wingspan/src/main/java/fs3/wingspan/repository/ImageRepository.java
+++ backend/wingspan/src/main/java/fs3/wingspan/repository/ImageRepository.java
@@ -1,4 +1,11 @@
 package fs3.wingspan.repository;
 
-public class ImageRepository {
+import fs3.wingspan.model.Image;
+import fs3.wingspan.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    Image findByPath(String path);
+    boolean existsByPath(String path);
+    Image findImageByTag(String tag);
 }
