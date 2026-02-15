ed36ab3bf7432c54831e16eeb5542475763c1887
ajvdb <ajvdb22@iastate.edu>
2026-02-15 13:28:55 -0600

    made tags model

--- backend/wingspan/src/main/java/fs3/wingspan/repository/ImageRepository.java
+++ backend/wingspan/src/main/java/fs3/wingspan/repository/ImageRepository.java
@@ -1,11 +1,11 @@
 package fs3.wingspan.repository;
 
 import fs3.wingspan.model.Image;
-import fs3.wingspan.model.Tag;
+import fs3.wingspan.model.Tags;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
     Image findById(Integer id);
     boolean existsByPath(String path);
-    Image findImageByTag(Tag tag);
+    Image findImageByTag(Tags tag);
 }
