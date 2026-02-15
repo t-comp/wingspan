a637c0ac3edd1cf0deca528bd147123b32359821
ajvdb <ajvdb22@iastate.edu>
2026-02-15 14:15:34 -0600

    fixed join tables and decided no need for image_tags entity

--- backend/wingspan/src/main/java/fs3/wingspan/repository/ImageTagsRepository.java
+++ backend/wingspan/src/main/java/fs3/wingspan/repository/ImageTagsRepository.java
@@ -1,7 +1,5 @@
 package fs3.wingspan.repository;
 
-import fs3.wingspan.model.Image;
-import fs3.wingspan.model.Image_tags;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageTagsRepository extends JpaRepository<Image_tags, Long> {
