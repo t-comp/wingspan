1dc94cbfa730af451e543e1152526a8312bea91c
ajvdb <ajvdb22@iastate.edu>
2026-03-17 13:39:10 -0500

    S3 config and hopefully allowing images to be accessed

---
+++ backend/wingspan/src/main/java/fs3/wingspan/config/S3Config.java
@@ -0,0 +1,35 @@
+package fs3.wingspan.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class S3Config {
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.cloud.aws.endpoint}")
+    private String endpoint;
+
+    @Bean
+    public S3Client S3Client(){
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                        .credentialsProvider(StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)))
+                .region(Region.of("us-east-1"))
+                .forcePathStyle(true)
+                .build();
+    }
+}
--- backend/wingspan/src/main/java/fs3/wingspan/services/ImageStorageService.java
+++ backend/wingspan/src/main/java/fs3/wingspan/services/ImageStorageService.java
@@ -14,7 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 import java.math.BigInteger;
@@ -46,6 +49,9 @@ public class ImageStorageService {
     @Autowired
     private TagRepository tagRepository;
 
+    @Autowired
+    private S3Client s3Client;
+
     /**
      * Save image file to disk and metadata to database
      */
@@ -61,11 +67,14 @@ public class ImageStorageService {
         String safeFilename = getSafeFilename(originalFilename);
         String uniqueFilename = UUID.randomUUID() + "_" + safeFilename + extension;
 
-        s3Template.upload(bucketName, uniqueFilename, file.getInputStream(),
-                ObjectMetadata.builder()
-                        .acl(ObjectCannedACL.PUBLIC_READ)
-                        .contentType(file.getContentType())
-                        .build());
+        PutObjectRequest putRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(uniqueFilename)
+                .contentType(file.getContentType())
+                .acl(ObjectCannedACL.PUBLIC_READ)  // ← makes file publicly accessible
+                .build();
+
+        s3Client.putObject(putRequest, RequestBody.fromBytes(file.getBytes()));
 
         String fileUrl = endpoint + "/" + bucketName + "/" + uniqueFilename;
 
