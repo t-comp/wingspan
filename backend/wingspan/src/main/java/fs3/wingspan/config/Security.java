d674efa8e042fc7552745a2f6e426bf7c52cb8f8
taysoupa <tbauer1@iastate.edu>
2026-03-29 18:51:25 -0500

    bri

--- backend/wingspan/src/main/java/fs3/wingspan/config/Security.java
+++ backend/wingspan/src/main/java/fs3/wingspan/config/Security.java
@@ -17,6 +17,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
 import java.util.List;
 
 @Configuration
